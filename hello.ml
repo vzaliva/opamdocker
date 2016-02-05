13fc46d7329d483124a3bfab28edce5c1bfd3ad0
Vadim Zaliva <lord@crocodile.org>
2016-02-04 17:15:14 -0800

    hello world

--- hello.ml
+++ hello.ml
@@ -1,3 +1,7 @@
-open Bat.List 
-print_string "Hello world!\n";;
+(* This is "Hello World" OCaml example.  To demonstrate dependencies
+from OPAM supplied libraries, we open Batteries here, although never
+use it *)
 
+open Batteries;;
+
+print_string "Hello world!\n";;
